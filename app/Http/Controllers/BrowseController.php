36987efb5dc6603e908b2a0ea971c98cb851631f
Cy <perfect.flight@gmail.com>
2019-12-29 00:06:43 +0800

    Convert to array

--- app/Http/Controllers/BrowseController.php
+++ app/Http/Controllers/BrowseController.php
@@ -94,11 +94,11 @@ class BrowseController extends Controller
         });
         // For feature and species searches, we're only searching by the current image of the character.
         if($request->get('feature_id')) {
-            $imageIds = DB::table('character_features')->where('feature_id', $request->get('feature_id'))->pluck('character_image_id');
+            $imageIds = DB::table('character_features')->where('feature_id', $request->get('feature_id'))->pluck('character_image_id')->toArray();
             $query->whereIn('character_image_id', array_unique($imageIds));
         }
         if($request->get('species_id')) {
-            $imageIds = DB::table('character_images')->where('species_id', $request->get('species_id'))->pluck('id');
+            $imageIds = DB::table('character_images')->where('species_id', $request->get('species_id'))->pluck('id')->toArray();
             $query->whereIn('character_image_id', $imageIds);
         }
         if($request->get('rarity_id')) $query->where('rarity_id', $request->get('rarity_id'));
