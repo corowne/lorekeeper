4b0c0af94d628ede7904dc1a84a168fb189e7572
itinerare <adestus@gmail.com>
2020-05-29 18:24:44 -0400

    Further adjustment of sidebar.

--- resources/views/prompts/_sidebar.blade.php
+++ resources/views/prompts/_sidebar.blade.php
@@ -2,7 +2,7 @@
     <li class="sidebar-header"><a =class="card-link"><a href="{{ url('prompts') }}">Prompts</a></a></li>
     <li class="sidebar-section">
         <div class="sidebar-section-header">Prompts</div>
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories') }}" class="{{ set_active('world/prompt-categories*') }}">Prompt Categories</a></div>
-        <div class="sidebar-item"><a href="{{ url('prompts/prompts') }}" class="{{ set_active('world/prompts*') }}">All Prompts</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories') }}" class="{{ set_active('prompts/prompt-categories*') }}">Prompt Categories</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompts') }}" class="{{ set_active('prompts/prompts*') }}">All Prompts</a></div>
     </li>
 </ul>
