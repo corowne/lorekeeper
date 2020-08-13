738861337d08d88f61e6189f8f978cd5818539fa
itinerare <adestus@gmail.com>
2020-08-12 21:09:47 -0400

    Additional checking.

--- app/Http/Controllers/HuntController.php
+++ app/Http/Controllers/HuntController.php
@@ -29,13 +29,15 @@ class HuntController extends Controller
         $hunt = ScavengerHunt::find($id);
         if(!$hunt) abort(404);
         $participantLog = HuntParticipant::where('hunt_id', $id)->where('user_id', Auth::user()->id)->first();
-        $logArray = [1 => $participantLog['target_1'], 2 => $participantLog['target_2'], 3 => $participantLog['target_3'], 4 => $participantLog['target_4'], 5 => $participantLog['target_5'], 6 => $participantLog['target_6'], 7 => $participantLog['target_7'], 8 => $participantLog['target_8'], 9 => $participantLog['target_9'], 10 => $participantLog['target_10']];
+        if($participantLog) {
+            $logArray = [1 => $participantLog['target_1'], 2 => $participantLog['target_2'], 3 => $participantLog['target_3'], 4 => $participantLog['target_4'], 5 => $participantLog['target_5'], 6 => $participantLog['target_6'], 7 => $participantLog['target_7'], 8 => $participantLog['target_8'], 9 => $participantLog['target_9'], 10 => $participantLog['target_10']];
+        }
         
         return view('scavenger_hunts.hunt', [
             'hunt' => $hunt,
             'targets' => HuntTarget::where('hunt_id', $id),
             'participantLog' => $participantLog,
-            'logArray' => $logArray,
+            'logArray' => ($participantLog && isset($logArray)) ? $logArray : null,
         ]);
     }
     
