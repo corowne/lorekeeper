ccb90424b95d719bc30683ab29dff885351b8b51
itinerare <adestus@gmail.com>
2020-09-05 16:52:19 -0400

    Migration

---
+++ database/migrations/2020_09_03_175532_add_character_drop_tables.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCharacterDropTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 
+        Schema::create('character_drop_data', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+
+            // If subtype is selected, drops will be available only for that subtype.
+            $table->integer('species_id')->unsigned();
+            $table->integer('subtype_id')->unsigned()->nullable();
+
+            // Will hold defined parameters and item data.
+            $table->text('parameters')->nullable()->default(null);
+            $table->text('data')->nullable()->default(null);
+        });
+
+        Schema::create('character_drops', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+
+            // Specific drop data being used, as well as associated character
+            $table->integer('drop_id')->unsigned();
+            $table->integer('character_id')->unsigned();
+
+            // Specific parameters associated with the individual character
+            $table->text('parameters')->nullable();
+
+            // Number of opportunities to collect the drops. Not equivalent to quantity.
+            $table->integer('drops_available')->unsigned()->default(0);
+
+            // Timestamp at which next drop becomes available
+            $table->timestamp('next_day')->nullable()->default(null);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('character_drop_data');
+        Schema::dropIfExists('character_drops');
+    }
+}
