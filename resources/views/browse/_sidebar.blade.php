f8e3fe86786999fa5b00f6596293b0b764277cd2
JuniJwi <junikimdraws@gmail.com>
2020-10-14 20:33:58 -0500

    format tweak for sidebar

--- resources/views/browse/_sidebar.blade.php
+++ resources/views/browse/_sidebar.blade.php
@@ -2,8 +2,11 @@
     <li class="sidebar-header"><a href="{{ url('masterlist') }}" class="card-link">Masterlist</a></li>
     <li class="sidebar-section">
         <div class="sidebar-section-header">Masterlist</div>
-        <div class="sidebar-item"><a href="{{ url('masterlist') }}" class="{{ set_active('masterlist*') }}">Character Masterlist</a></div>
-        <div class="sidebar-item"><a href="{{ url('myos') }}" class="{{ set_active('myos*') }}">MYO Slot Masterlist</a></div>
+        <div class="sidebar-item"><a href="{{ url('masterlist') }}" class="{{ set_active('masterlist*') }}">Characters</a></div>
+        <div class="sidebar-item"><a href="{{ url('myos') }}" class="{{ set_active('myos*') }}">MYO Slots</a></div>
+    </li>
+    <li class="sidebar-section">
+        <div class="sidebar-section-header">Sub Masterlists</div>
         @foreach($sublists as $sublist)
         <div class="sidebar-item"><a href="{{ url('sublist/'.$sublist->key) }}" class="{{ set_active('sublist/'.$sublist->key) }}">{{ $sublist->name }}</a></div>
         @endforeach
