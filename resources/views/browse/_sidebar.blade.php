8f743d7d6c4902b3e50307bec45596ab2d855fb7
JuniJwi <junikimdraws@gmail.com>
2020-10-19 02:30:20 -0500

    add checks to sidebar for if 0 sublists

--- resources/views/browse/_sidebar.blade.php
+++ resources/views/browse/_sidebar.blade.php
@@ -5,10 +5,12 @@
         <div class="sidebar-item"><a href="{{ url('masterlist') }}" class="{{ set_active('masterlist*') }}">Characters</a></div>
         <div class="sidebar-item"><a href="{{ url('myos') }}" class="{{ set_active('myos*') }}">MYO Slots</a></div>
     </li>
-    <li class="sidebar-section">
-        <div class="sidebar-section-header">Sub Masterlists</div>
-        @foreach($sublists as $sublist)
-        <div class="sidebar-item"><a href="{{ url('sublist/'.$sublist->key) }}" class="{{ set_active('sublist/'.$sublist->key) }}">{{ $sublist->name }}</a></div>
-        @endforeach
-    </li>
+    @if(isset($sublists) && $sublists->count() > 0)
+        <li class="sidebar-section">
+            <div class="sidebar-section-header">Sub Masterlists</div>
+            @foreach($sublists as $sublist)
+            <div class="sidebar-item"><a href="{{ url('sublist/'.$sublist->key) }}" class="{{ set_active('sublist/'.$sublist->key) }}">{{ $sublist->name }}</a></div>
+            @endforeach
+        </li>
+    @endif
 </ul>
