ea4f05da97e56134fa971339f5e5e84efe7e1e13
Uri <dare020dream@gmail.com>
2020-10-26 22:45:17 -0400

    - Adding commas and "and" between applicable lists of MultiItem - Changing item(s) to dynamically check if it's plural or not using quantity.

--- resources/views/home/crafting/_recipe_ingredient_entry.blade.php
+++ resources/views/home/crafting/_recipe_ingredient_entry.blade.php
@@ -3,21 +3,26 @@
         {{ $ingredient->quantity }} @if(isset($ingredient->ingredient->image_url))<img class="small-icon" src="{{ $ingredient->ingredient->image_url }}">@endif<span>{!! $ingredient->ingredient->displayName !!}</span>
         @break
     @case('MultiItem')
-        <p>Any mix of {{ $ingredient->quantity }} item(s) from the following:</p>
-        @foreach($ingredient->ingredient as $ing)
-            <div>- @if(isset($ing->image_url))<img class="small-icon" src="{{ $ing->image_url }}">@endif<span>{!! $ing->displayName !!}</span></div>
-        @endforeach
+        <strong>Any mix of {{ $ingredient->quantity }} item{{ $ingredient->quantity == 1 ? '' : 's'}} from the following:</strong>
+        <p class="mb-0">
+            @foreach($ingredient->ingredient as $key => $ing)
+                @if(isset($ing->image_url))<img class="small-icon" src="{{ $ing->image_url }}">@endif <strong>{!! $ing->displayName !!}</strong>{{ ($key < $ingredient->ingredient->count()-1) && ($ingredient->ingredient->count() > 2) ? ', ' : '' }}{{ ($key == $ingredient->ingredient->count()-2) && ($ingredient->ingredient->count() > 1) ? ' and ' : '' }}
+            @endforeach
+        </p>
         @break
     @case('Category')
-        {{ $ingredient->quantity }} item(s) from the 
+        {{ $ingredient->quantity }} item{{ $ingredient->quantity == 1 ? '' : 's'}} from the 
         @if(isset($ingredient->ingredient->image_url))<img class="small-icon" src="{{ $ingredient->ingredient->image_url }}">@endif{!! $ingredient->ingredient->displayName !!} 
         category
         @break
     @case('MultiCategory')
-        Any mix of {{ $ingredient->quantity }} item(s) from the following categories:</p>
-        @foreach($ingredient->ingredient as $ing)
-            <div>- @if(isset($ing->image_url))<img class="small-icon" src="{{ $ing->image_url }}">@endif<span>{!! $ing->displayName !!}</div>
-        @endforeach
+        <!-- This doesn't work yet! -->
+        <strong>Any mix of {{ $ingredient->quantity }} item{{ $ingredient->quantity == 1 ? '' : 's'}} from the following categories:</strong>
+        <p class="mb-0">
+            @foreach($ingredient->ingredient as $ing)
+                <div>- @if(isset($ing->image_url))<img class="small-icon" src="{{ $ing->image_url }}">@endif<span>{!! $ing->displayName !!}</div>
+            @endforeach
+        </p>
     @case('Currency')
         {{ $ingredient->quantity }} @if(isset($ingredient->image_url))<img class="small-icon" src="{{ $ingredient->ingredient->image_url }}">@endif<span>{!! $ingredient->ingredient->displayName !!}</span>
         @break
