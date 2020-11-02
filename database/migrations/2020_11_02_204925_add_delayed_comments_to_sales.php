06b3dbd8602306cf32a38e0d5814774b94c1b115
itinerare <adestus@gmail.com>
2020-11-02 17:42:37 -0500

    Trim migration

--- database/migrations/2020_11_02_204925_add_delayed_comments_to_sales.php
+++ database/migrations/2020_11_02_204925_add_delayed_comments_to_sales.php
@@ -14,13 +14,8 @@ class AddDelayedCommentsToSales extends Migration
     public function up()
     {
         Schema::table('sales', function (Blueprint $table) {
-            // Time at which comments should open to members
             $table->timestamp('comments_open_at')->nullable()->default(null);
-            
-            // Bools
             $table->boolean('is_open')->default(1);
-            $table->boolean('hide_comments_before_start')->default(0);
-            $table->boolean('disable_commenting_after_close')->default(0);
         });
     }
 
@@ -35,8 +30,6 @@ class AddDelayedCommentsToSales extends Migration
             //
             $table->dropColumn('is_open');
             $table->dropColumn('comments_open_at');
-            $table->dropColumn('hide_comments_before_start');
-            $table->dropColumn('disable_commenting_after_close');
         });
     }
 }
