d0efa2c76206013045715e78a96be7d4fb649d07
Dee <deep.atora@gmail.com>
2020-11-26 10:45:19 +1030

    view duplication trimming

--- resources/views/admin/characters/create_edit_character_category.blade.php
+++ resources/views/admin/characters/create_edit_character_category.blade.php
@@ -47,33 +47,10 @@
     {!! Form::textarea('description', $category->description, ['class' => 'form-control wysiwyg']) !!}
 </div>
 
-<?php
-$vals = [true, false, false];
-if (isset($lineageBlacklist))
-{
-    $vals[0] = false;
-    $vals[$lineageBlacklist->complete_removal ? 2 : 1] = true;
-}
-?>
-<h3>Lineage Blacklist</h3>
-<div class="form-check mb-1">
-  <label class="form-check-label">
-    {!! Form::radio('lineage-blacklist', '0', $vals[0], ['class' => 'mr-1']) !!}
-    No restriction. <span class="text-muted font-italic">Will have lineage as long as the species and subtype also allow it.</span>
-  </label>
-</div>
-<div class="form-check mb-1">
-  <label class="form-check-label">
-    {!! Form::radio('lineage-blacklist', '1', $vals[1], ['class' => 'mr-1']) !!}
-    Characters in this category can have ancestors but not descendants. <span class="text-muted font-italic">Such as mules, hybrids, children, etc.</span>
-  </label>
-</div>
-<div class="form-check disabled mb-2">
-  <label class="form-check-label">
-    {!! Form::radio('lineage-blacklist', '2', $vals[2], ['class' => 'mr-1']) !!}
-    Characters in this category cannot have lineages at all. <span class="text-muted font-italic">Such as locations, artifacts, etc.</span>
-  </label>
-</div>
+@include('admin.lineage._edit_lineage_blacklist', [
+    'lineageBlacklist' => $lineageBlacklist,
+    'type' => 'category'
+])
 
 <div class="text-right">
     {!! Form::submit($category->id ? 'Edit' : 'Create', ['class' => 'btn btn-primary']) !!}
---
+++ resources/views/admin/lineage/_edit_lineage_blacklist.blade.php
@@ -0,0 +1,35 @@
+<?php
+$vals = [true, false, false];
+if (isset($lineageBlacklist))
+{
+    $vals[0] = false;
+    $vals[$lineageBlacklist->complete_removal ? 2 : 1] = true;
+}
+$opts = ['category', 'species', 'subtype', 'rarity'];
+foreach ($opts as $key => $value) {
+    if ($value == $type) unset($opts[$key]);
+}
+$opts = array_values($opts);
+?>
+<h3>Lineage Blacklist</h3>
+<div class="form-check mb-1">
+    <label class="form-check-label">
+        {!! Form::radio('lineage-blacklist', '0', $vals[0], ['class' => 'mr-1']) !!}
+        No restriction.
+        <span class="text-muted font-italic">
+            Characters will have lineage as long as the {{ $opts[0] }}, {{ $opts[1] }} and {{ $opts[2] }} also allow it.
+        </span>
+    </label>
+</div>
+<div class="form-check mb-1">
+  <label class="form-check-label">
+    {!! Form::radio('lineage-blacklist', '1', $vals[1], ['class' => 'mr-1']) !!}
+    Characters with this {{ $type }} can have ancestors but not descendants. <span class="text-muted font-italic">Such as mules, hybrids, children, etc.</span>
+  </label>
+</div>
+<div class="form-check disabled mb-2">
+  <label class="form-check-label">
+    {!! Form::radio('lineage-blacklist', '2', $vals[2], ['class' => 'mr-1']) !!}
+    Characters with this {{ $type }} cannot have lineages at all. <span class="text-muted font-italic">Such as locations, artifacts, etc.</span>
+  </label>
+</div>
