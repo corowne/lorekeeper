70d22e71cc0ea8b8070979b5d6c97301ca9a44e9
itinerare <adestus@gmail.com>
2020-11-26 00:01:43 -0500

    Fix item entry expansion item entry ifs to use shops count

--- resources/views/world/_item_entry.blade.php
+++ resources/views/world/_item_entry.blade.php
@@ -42,7 +42,7 @@
         <div class="world-entry-text">
             @if(isset($item->reference) && $item->reference && Config::get('lorekeeper.extensions.item_entry_expansion.extra_fields'))  <p><strong>Reference Link:</strong> <a href="{{ $item->reference }}">{{ $item->reference }}</a></p> @endif
             {!! $description !!}
-            @if((isset($item->uses) && $item->uses || isset($item->source) && $item->source || isset($item->data['shops']) && $item->data['shops'] || isset($item->data['prompts']) && $item->data['prompts']) && Config::get('lorekeeper.extensions.item_entry_expansion.extra_fields'))
+            @if((isset($item->uses) && $item->uses || isset($item->source) && $item->source || $shops->count() || isset($item->data['prompts']) && $item->data['prompts']) && Config::get('lorekeeper.extensions.item_entry_expansion.extra_fields'))
             <div class="text-right"><a data-toggle="collapse" href="#item-{{ $item->id }}" class="text-primary"><strong>Show details...</strong></a></div>
             <div class="collapse" id="item-{{ $item->id }}">
                 @if(isset($item->uses) && $item->uses)  <p><strong>Uses:</strong> {{ $item->uses }}</p> @endif
@@ -51,11 +51,11 @@
                 <div class="row">
                     @if(isset($item->source) && $item->source)
                         <div class="col">
-                            <p><strong>Source:</strong></p> 
+                            <p><strong>Source:</strong></p>
                             <p>{!! $item->source !!}</p>
                         </div>
                     @endif
-                    @if(isset($item->data['shops']) && $item->data['shops'])
+                    @if($shops->count())
                         <div class="col">
                             <p><strong>Purchaseable At:</strong></p>
                                 <div class="row">
