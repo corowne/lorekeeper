21cff109cffa5ba2610593a33ee08a5bf90f8344
itinerare <adestus@gmail.com>
2020-11-28 15:56:32 -0500

    Add display of user currencies to shops sidebar

--- resources/views/shops/_sidebar.blade.php
+++ resources/views/shops/_sidebar.blade.php
@@ -1,10 +1,14 @@
 <ul>
     <li class="sidebar-header"><a href="{{ url('shops') }}" class="card-link">Shops</a></li>
-    
+
     @if(Auth::check())
         <li class="sidebar-section">
             <div class="sidebar-section-header">History</div>
             <div class="sidebar-item"><a href="{{ url('shops/history') }}" class="{{ set_active('shops/history') }}">My Purchase History</a></div>
+            <div class="sidebar-section-header">My Currencies</div>
+            @foreach(Auth::user()->getCurrencies(true) as $currency)
+                <div class="sidebar-item pr-3">{!! $currency->display($currency->quantity) !!}</div>
+            @endforeach
         </li>
     @endif
 
