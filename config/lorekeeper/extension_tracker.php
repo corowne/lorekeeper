ccad659afe0c76a663d1e2cb42d4fcfae77626b1
itinerare <adestus@gmail.com>
2020-11-30 11:28:39 -0500

    Add extension tracker info

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    
+
     // FORMAT EXAMPLE.
     //
     // 'extension_tracker' => [
@@ -11,6 +11,14 @@ return [
     //     ]),
     //     'version' => '1.0.0',
     // ],
-    
+
+    'character_drops' => [
+        'key' => 'character_drops',
+        'wiki_key' => 'Character_Drops',
+        'creators' => json_encode([
+            'Mercury' => 'https://github.com/itinerare/',
+        ]),
+        'version' => '1.0.0',
+        ],
 
 ];
