24c247c0ba7ae7f357dd701a0ec87903f6dfa925
Draginraptor <draginraptor@gmail.com>
2021-01-13 22:52:42 +0800

    - Added details to extension_tracker - Fixed POST recipe craft form url

--- resources/views/home/crafting/_modal_craft.blade.php
+++ resources/views/home/crafting/_modal_craft.blade.php
@@ -29,7 +29,7 @@
         </div>
     </div>
     {{-- Check if sufficient ingredients have been selected? --}}
-    {!! Form::open(['url' => 'crafting/craft']) !!}
+    {!! Form::open(['url' => 'crafting/craft/'.$recipe->id]) !!}
         @include('widgets._inventory_select', ['user' => Auth::user(), 'inventory' => $inventory, 'categories' => $categories, 'selected' => $selected, 'page' => $page])
         <div class="text-right">
             {!! Form::submit('Craft', ['class' => 'btn btn-primary']) !!}
