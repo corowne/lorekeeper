fb9efda134808d648d8a5f567ce6e10a2a68d214
Draginraptor <draginraptor@gmail.com>
2021-08-02 04:04:12 +0800

    style: 'Level UP' to 'Level Up'; Character count log rows now refer to 'count' instead of 'exp'

--- resources/views/character/stats/_count_log_row.blade.php
+++ resources/views/character/stats/_count_log_row.blade.php
@@ -1,10 +1,10 @@
-<tr class="{{ ($exp->recipient_id == $character->id) ? 'inflow' : 'outflow' }}">
-    <td><i class="btn py-1 m-0 px-2 btn-{{ ($exp->quantity > 0 ) ? 'success' : 'danger'}} fas {{ ($exp->quantity > 0 ) ? 'fa-arrow-up' : 'fa-arrow-down' }} mr-2"></i>
-      {!! $exp->sender ? $exp->sender->displayName : '' !!}
+<tr class="{{ ($count->recipient_id == $character->id) ? 'inflow' : 'outflow' }}">
+    <td><i class="btn py-1 m-0 px-2 btn-{{ ($count->quantity > 0 ) ? 'success' : 'danger'}} fas {{ ($count->quantity > 0 ) ? 'fa-arrow-up' : 'fa-arrow-down' }} mr-2"></i>
+      {!! $count->sender ? $count->sender->displayName : '' !!}
     </td>
-    <td>{!! $exp->recipient ? $exp->recipient->displayName : '' !!}</div>
-    <td>{{  $exp->quantity }}</div>
-    <td>{!! $exp->log !!}</div>
-    <td>{!! pretty_date($exp->created_at) !!}</div>
+    <td>{!! $count->recipient ? $count->recipient->displayName : '' !!}</div>
+    <td>{{  $count->quantity }}</div>
+    <td>{!! $count->log !!}</div>
+    <td>{!! pretty_date($count->created_at) !!}</div>
 </tr>
   
