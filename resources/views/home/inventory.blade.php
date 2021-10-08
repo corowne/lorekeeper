9ccf4b39f9618f99e867463133dd7a9c8f5ac992
itinerare <adestus@gmail.com>
2021-10-08 15:13:10 -0400

    fix(inventory): item image issue

--- resources/views/home/inventory.blade.php
+++ resources/views/home/inventory.blade.php
@@ -25,7 +25,7 @@
                 <div class="row mb-3">
                     @foreach($chunk as $itemId=>$stack)
                         <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $stack->first()->pivot->id }}" data-name="{{ $user->name }}'s {{ $stack->first()->name }}">
-                            @if($stack->first()->hasImage)
+                            @if($stack->first()->has_image)
                                 <div class="mb-1">
                                     <a href="#" class="inventory-stack"><img src="{{ $stack->first()->imageUrl }}" alt="{{ $stack->first()->name }}"/></a>
                                 </div>
