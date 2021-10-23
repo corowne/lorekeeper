acf725a5b0cfd7f6f0e0962db8636a0ed20aeae2
Cy <perfect.flight@gmail.com>
2021-10-23 22:38:06 +0800

    WIP table styling, fixing indentation

--- resources/views/character/ownership_logs.blade.php
+++ resources/views/character/ownership_logs.blade.php
@@ -16,16 +16,20 @@
 <h3>Ownership History</h3>
 
 {!! $logs->render() !!}
-<div class="row ml-md-2 mb-4">
-  <div class="d-flex row flex-wrap col-12 mt-1 pt-1 px-0 ubt-bottom">
-    <div class="col-6 col-md font-weight-bold">Sender</div>
-    <div class="col-6 col-md font-weight-bold">Recipient</div>
-    <div class="col-6 col-md-4 font-weight-bold">Log</div>
-    <div class="col-6 col-md font-weight-bold">Date</div>
-  </div>
-    @foreach($logs as $log)
-        @include('user._ownership_log_row', ['log' => $log, 'user' => $character->user])
-    @endforeach
+<div class="mb-4 logs-table">
+    <div class="row logs-table-header">
+        <div class="col-6 col-md"><div class="logs-table-cell">Sender</div></div>
+        <div class="col-6 col-md"><div class="logs-table-cell">Recipient</div></div>
+        <div class="col-6 col-md-4"><div class="logs-table-cell">Log</div></div>
+        <div class="col-6 col-md"><div class="logs-table-cell">Date</div></div>
+    </div>
+    <div class="logs-table-body">
+        @foreach($logs as $log)
+            <div class="logs-table-row">
+                @include('user._ownership_log_row', ['log' => $log, 'user' => $character->user])
+            </div>
+        @endforeach
+    </div>
 </div>
 {!! $logs->render() !!}
 
