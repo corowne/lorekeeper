70c5e83fdbfb24e96895304d8180247492ae0778
Cy <perfect.flight@gmail.com>
2022-04-17 18:00:42 +0800

    Checking the params

--- app/Providers/Socialite/ToyhouseProvider.php
+++ app/Providers/Socialite/ToyhouseProvider.php
@@ -18,7 +18,7 @@ class ToyhouseProvider extends AbstractProvider implements ProviderInterface
      */
     protected function getAuthUrl($state)
     {
-        dd($state);
+        dd($this->buildAuthUrlFromBase('https://toyhou.se/~oauth/authorize', $state));
         return $this->buildAuthUrlFromBase('https://toyhou.se/~oauth/authorize', $state);
     }
 
