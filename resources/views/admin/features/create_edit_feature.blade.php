98ed7868cb5262b2299c3f1da803f3747fb2d848
itinerare <itinerare@users.noreply.github.com>
2022-08-29 16:16:33 +0000

    refactor: fix blade formatting

--- resources/views/admin/features/create_edit_feature.blade.php
+++ resources/views/admin/features/create_edit_feature.blade.php
@@ -69,14 +69,14 @@
         {!! Form::textarea('description', $feature->description, ['class' => 'form-control wysiwyg']) !!}
     </div>
 
-<div class="form-group">
-    {!! Form::checkbox('is_visible', 1, $feature->id ? $feature->is_visible : 1, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
-    {!! Form::label('is_visible', 'Is Visible', ['class' => 'form-check-label ml-3']) !!} {!! add_help('If turned off, the shop will not be visible in the trait list or available for selection in search and design updates. Permissioned staff will still be able to add them to characters, however.') !!}
-</div>
+    <div class="form-group">
+        {!! Form::checkbox('is_visible', 1, $feature->id ? $feature->is_visible : 1, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
+        {!! Form::label('is_visible', 'Is Visible', ['class' => 'form-check-label ml-3']) !!} {!! add_help('If turned off, the shop will not be visible in the trait list or available for selection in search and design updates. Permissioned staff will still be able to add them to characters, however.') !!}
+    </div>
 
-<div class="text-right">
-    {!! Form::submit($feature->id ? 'Edit' : 'Create', ['class' => 'btn btn-primary']) !!}
-</div>
+    <div class="text-right">
+        {!! Form::submit($feature->id ? 'Edit' : 'Create', ['class' => 'btn btn-primary']) !!}
+    </div>
 
     {!! Form::close() !!}
 
@@ -101,4 +101,3 @@
         });
     </script>
 @endsection
-
