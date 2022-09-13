a5977c8cf3cd3fb2cfc2eb3a56b94b69546d9519
SpeedyD <speedydvv@gmail.com>
2022-09-14 00:47:53 +0200

    Hide thumbnail upload for users feature (Addition on previous automation, edited credits to reflect.)

--- resources/views/pages/credits.blade.php
+++ resources/views/pages/credits.blade.php
@@ -60,8 +60,10 @@
             <a href="http://wiki.lorekeeper.me/index.php?title=Extensions:User_Transfer_Reasons"><strong>User Transfer Reasons</strong></a> by <a href="https://github.com/snupsplus">Snupsplus</a>
         </p>
         <p class="mb-0 col-md-4">
-            <a href="http://wiki.lorekeeper.me/index.php?title=Extensions:Watermarking"><strong>Watermarking</strong></a> by <a href="https://github.com/itinerare">itinerare</a> with masterlist image automation by <a
-                href="https://github.com/SpeedyD">Speedy</a>
+            <a href="http://wiki.lorekeeper.me/index.php?title=Extensions:Watermarking"><strong>Watermarking</strong></a> by <a href="https://github.com/itinerare">itinerare</a>
+        </p>
+		<p class="mb-0 col-md-4">
+            <strong>Watermarking - Masterlist Image Automation addition</strong></a> by <a href="https://github.com/SpeedyD">Speedy</a> 
         </p>
 
         <p class="mb-0 col-md-4">
