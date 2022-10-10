81e61f62db7d20165f2845ed40a080a63766a714
JAM\mallo <mallowmagicalus@Gmail.com>
2022-10-10 17:32:29 -0400

    adding sidebar and index links

--- resources/views/world/index.blade.php
+++ resources/views/world/index.blade.php
@@ -19,6 +19,7 @@
                 <li class="list-group-item"><a href="{{ url('world/rarities') }}">Rarities</a></li>
                 <li class="list-group-item"><a href="{{ url('world/trait-categories') }}">Trait Categories</a></li>
                 <li class="list-group-item"><a href="{{ url('world/traits') }}">All Traits</a></li>
+                <li class="list-group-item"><a href="{{ url('world/universaltraits') }}">Universal Trait Index</a></li>
                 <li class="list-group-item"><a href="{{ url('world/character-categories') }}">Character Categories</a></li>
             </ul>
         </div>
