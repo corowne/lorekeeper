dec005d2380c613fdd21be5f0694b6e641e4ee67
Ne-wt <Ne-wt@users.noreply.github.com>
2023-01-09 21:55:27 +0000

    refactor: fix blade formatting

--- resources/views/comments/_comment.blade.php
+++ resources/views/comments/_comment.blade.php
@@ -3,11 +3,11 @@
     $markdown->setSafeMode(true);
 @endphp
 
-@if($comment->deleted_at == null)
+@if ($comment->deleted_at == null)
     <div id="comment-{{ $comment->getKey() }}" class="{{ isset($reply) && $reply === true ? 'comment_replies border-left col-12 column mw-100 pr-0' : '' }} pt-4" style="flex-basis: 100%;">
         <div class="media-body row mw-100 mx-0" style="flex:1;flex-wrap:wrap;">
             {{-- Show avatar if not compact --}}
-            @if(isset($compact) && !$compact)
+            @if (isset($compact) && !$compact)
                 <div class="d-none d-md-block">
                     <img class="mr-3 mt-2" src="/images/avatars/{{ $comment->commenter->avatar }}" style="width:70px; height:70px; border-radius:50%;" alt="{{ $comment->commenter->name }} Avatar">
                 </div>
@@ -19,31 +19,34 @@
                 {{-- Comment block header --}}
                 <div class="row mx-0 px-0 align-items-md-end">
                     <h5 class="mt-0 mb-1 col mx-0 px-0">
-                        {!! $comment->commenter->commentDisplayName !!} @if($comment->commenter->isStaff == true)<small class="text-success">Staff Member</small>@endif
+                        {!! $comment->commenter->commentDisplayName !!} @if ($comment->commenter->isStaff == true)
+                            <small class="text-success">Staff Member</small>
+                        @endif
                     </h5>
-                    @if($comment->is_featured)
+                    @if ($comment->is_featured)
                         <div class="ml-1 text-muted text-right col-6 mx-0 pr-1"><small class="text-success">Featured by Owner</small></div>
                     @endif
                 </div>
 
                 {{-- Comment --}}
-                <div class="comment border p-3 rounded {{ $comment->is_featured ? 'border-success bg-light' : '' }} {{ $comment->likes()->where('is_like', 1)->count() - $comment->likes()->where('is_like', 0)->count() < 0 ? 'bg-light bg-gradient' : '' }}">
+                <div
+                    class="comment border p-3 rounded {{ $comment->is_featured ? 'border-success bg-light' : '' }} {{ $comment->likes()->where('is_like', 1)->count() -$comment->likes()->where('is_like', 0)->count() <0? 'bg-light bg-gradient': '' }}">
                     <p>{!! nl2br($markdown->line($comment->comment)) !!} </p>
                     <p class="border-top pt-1 text-right mb-0">
                         <small class="text-muted">{!! $comment->created_at !!}
-                            @if($comment->created_at != $comment->updated_at)
-                                <span class="text-muted border-left mx-1 px-1">(Edited {!! ($comment->updated_at) !!})</span>
+                            @if ($comment->created_at != $comment->updated_at)
+                                <span class="text-muted border-left mx-1 px-1">(Edited {!! $comment->updated_at !!})</span>
                             @endif
                         </small>
-                        @if($comment->type == "User-User")
-                            <a href="{{ url('comment/').'/'.$comment->id }}"><i class="fas fa-link ml-1" style="opacity: 50%;"></i></a>
+                        @if ($comment->type == 'User-User')
+                            <a href="{{ url('comment/') . '/' . $comment->id }}"><i class="fas fa-link ml-1" style="opacity: 50%;"></i></a>
                         @endif
                         <a href="{{ url('reports/new?url=') . $comment->url }}"><i class="fas fa-exclamation-triangle" data-toggle="tooltip" title="Click here to report this comment." style="opacity: 50%;"></i></a>
                     </p>
                 </div>
 
                 @include('comments._actions', ['comment' => $comment, 'compact' => isset($compact) ? $compact : false])
-            
+
             </div>
 
             {{-- Recursion for children --}}
--- resources/views/comments/_form.blade.php
+++ resources/views/comments/_form.blade.php
@@ -1,11 +1,11 @@
 <div class="card mt-3">
     <div class="card-body">
-        {!! Form::open(['url' => 'comments/make/'.base64_encode(urlencode(get_class($model))).'/'.$model->getKey()]) !!}
+        {!! Form::open(['url' => 'comments/make/' . base64_encode(urlencode(get_class($model))) . '/' . $model->getKey()]) !!}
         <div class="form-group">
             {!! Form::label('message', 'Enter your message here:') !!}
             {!! Form::textarea('message', null, ['class' => 'form-control', 'rows' => 3, 'required']) !!}
             <small class="form-text text-muted"><a target="_blank" href="https://help.github.com/articles/basic-writing-and-formatting-syntax">Markdown</a> cheatsheet.</small>
-        </div> 
+        </div>
 
         {!! Form::submit('Submit', ['class' => 'btn btn-sm btn-outline-success text-uppercase']) !!}
         {!! Form::close() !!}
