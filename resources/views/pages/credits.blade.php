fe056088ff5675f9c3be6df57901705b5a5902d5
SpeedyD <SpeedyD@users.noreply.github.com>
2023-01-14 17:31:27 +0000

    refactor: fix blade formatting

--- resources/views/pages/credits.blade.php
+++ resources/views/pages/credits.blade.php
@@ -68,7 +68,7 @@
         <p class="mb-0 col-md-4">
             <strong>Watermarking - Masterlist Image Automation addition</strong></a> by <a href="https://github.com/SpeedyD">Speedy</a>
         </p>
-		<p class="mb-0 col-md-4">
+        <p class="mb-0 col-md-4">
             <a href="http://wiki.lorekeeper.me/index.php?title=Extensions:90_Percent_Watermark"><strong>Watermark Resizing</strong></a> by <a href="https://github.com/SpeedyD">Speedy</a>
         </p>
 
