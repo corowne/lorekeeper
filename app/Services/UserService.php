54c39df9932fac48efabc7e9bf24c6dfdcd55834
moif <moiftab@gmail.com>
2023-02-07 22:08:55 -0500

    Bug: Fix the admin create script to set has_alias

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -51,6 +51,7 @@ class UserService extends Service {
             'rank_id'  => $data['rank_id'],
             'password' => Hash::make($data['password']),
             'birthday' => $formatDate,
+            'has_alias' => isset($data['has_alias']) ? $data['has_alias'] : null,
         ]);
         $user->settings()->create([
             'user_id' => $user->id,
