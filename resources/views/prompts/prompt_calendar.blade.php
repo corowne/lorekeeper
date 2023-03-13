a4050108b27a263c0d6e2b4bb1491a7fa2fe9e68
SpeedyD <SpeedyD@users.noreply.github.com>
2023-03-13 12:30:27 +0000

    refactor: fix blade formatting

--- resources/views/prompts/prompt_calendar.blade.php
+++ resources/views/prompts/prompt_calendar.blade.php
@@ -1,36 +1,34 @@
 @extends('prompts.layout')
 
-@section('title') Prompt Calendar @endsection
+@section('title')
+    Prompt Calendar
+@endsection
 
 @section('content')
-{!! breadcrumbs(['Prompts' => 'prompts', 'Prompt Calendar ' => 'prompts/prompt-calendar']) !!}
-<h1>Prompt Calendar</h1>
+    {!! breadcrumbs(['Prompts' => 'prompts', 'Prompt Calendar ' => 'prompts/prompt-calendar']) !!}
+    <h1>Prompt Calendar</h1>
 
-<div id='calendar'></div>
+    <div id='calendar'></div>
 
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.js'></script>
     <script>
-
-      document.addEventListener('DOMContentLoaded', function() {
-        const calendarEl = document.getElementById('calendar')
-        const calendar = new FullCalendar.Calendar(calendarEl, {
-			initialView: 'multiMonthYear',
-			multiMonthMaxColumns: 2,
-			lazyFetching: true,
-			headerToolbar: {
-				left: 'prev,next',
-				center: 'title',
-				right: 'multiMonthYear timeGridWeek timeGridDay',
-			},
-			eventSources: [
-				{
-					url: 'prompt-json',
-					format: 'json',
-				}
-			]
+        document.addEventListener('DOMContentLoaded', function() {
+            const calendarEl = document.getElementById('calendar')
+            const calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'multiMonthYear',
+                multiMonthMaxColumns: 2,
+                lazyFetching: true,
+                headerToolbar: {
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'multiMonthYear timeGridWeek timeGridDay',
+                },
+                eventSources: [{
+                    url: 'prompt-json',
+                    format: 'json',
+                }]
+            })
+            calendar.render()
         })
-        calendar.render()
-      })
     </script>
-
 @endsection
