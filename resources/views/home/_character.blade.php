cab8d34635ed6ef3d2c7478e52b8645da5d5188e
SpeedyD <SpeedyD@users.noreply.github.com>
2023-05-19 16:34:45 +0000

    refactor: fix blade formatting

--- resources/views/home/_character.blade.php
+++ resources/views/home/_character.blade.php
@@ -1,7 +1,7 @@
 @if ($character)
     <div class="character-info" data-id="{{ $character->id }}"><img src="{{ $character->image->thumbnailUrl }}" class="mw-100" alt="Thumbnail for {{ $character->fullName }}" /></div>
     <div class="text-center"><a href="{{ $character->url }}">{{ $character->slug }}</a></div>
-    @if(!$character->is_visible && Auth::user()->isStaff)
+    @if (!$character->is_visible && Auth::user()->isStaff)
         <div class="text-danger character-info" data-id="0"><i class="fas fa-eye-slash mr-1"></i> Character hidden from public view.</div>
     @endif
 @else
