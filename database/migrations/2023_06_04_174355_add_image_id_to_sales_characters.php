23dede8b9c8be7c90d30e6f5e0fa240d5a52e5df
AW0005 <AW0005@users.noreply.github.com>
2023-06-04 19:06:37 +0000

    refactor: fix PHP styling

--- database/migrations/2023_06_04_174355_add_image_id_to_sales_characters.php
+++ database/migrations/2023_06_04_174355_add_image_id_to_sales_characters.php
@@ -7,8 +7,6 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up() {
         Schema::table('sales_characters', function (Blueprint $table) {
@@ -18,8 +16,6 @@ return new class extends Migration {
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down() {
         Schema::table('sales_characters', function (Blueprint $table) {
