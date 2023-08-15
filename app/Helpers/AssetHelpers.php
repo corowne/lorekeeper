2ce9e1f8b25123da9da7525fac26b99a78388909
Ne-wt <Ne-wt@users.noreply.github.com>
2023-08-15 00:57:45 +0000

    refactor: fix PHP styling

--- app/Helpers/AssetHelpers.php
+++ app/Helpers/AssetHelpers.php
@@ -369,17 +369,20 @@ function fillCharacterAssets($assets, $sender, $recipient, $logType, $data, $sub
 /**
  * Creates a rewards string from an asset array.
  *
- * @param  array  $array
+ * @param array $array
+ *
  * @return string
  */
-function createRewardsString($array)
-{
+function createRewardsString($array) {
     $string = [];
-    foreach($array as $key => $contents)
-    {
-        foreach($contents as $asset)
-            $string[] = $asset['asset']->displayName . ' x'.$asset['quantity'];
+    foreach ($array as $key => $contents) {
+        foreach ($contents as $asset) {
+            $string[] = $asset['asset']->displayName.' x'.$asset['quantity'];
+        }
     }
-    if (!count ($string)) return;
-    return implode(', ', array_slice($string, 0, count($string) - 1)) . (count($string) > 2 ? ', and ' : ' and ') . end($string);
+    if (!count($string)) {
+        return;
+    }
+
+    return implode(', ', array_slice($string, 0, count($string) - 1)).(count($string) > 2 ? ', and ' : ' and ').end($string);
 }
