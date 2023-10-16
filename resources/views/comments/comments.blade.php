530b75b2115927dde857e390f96cbcd38d5b6263
itinerare <itinerare@users.noreply.github.com>
2023-10-16 12:46:11 +0000

    refactor: fix blade formatting

--- resources/views/comments/comments.blade.php
+++ resources/views/comments/comments.blade.php
@@ -20,24 +20,24 @@
 <div class="d-flex mw-100 row mx-0" style="overflow:hidden;">
     @php
         $comments = $comments->sortByDesc('created_at');
-        
+
         if (isset($perPage)) {
             $page = request()->query('page', 1) - 1;
-        
+
             $parentComments = $comments->where('child_id', '');
-        
+
             $slicedParentComments = $parentComments->slice($page * $perPage, $perPage);
-        
+
             $m = Config::get('comments.model'); // This has to be done like this, otherwise it will complain.
             $modelKeyName = (new $m())->getKeyName(); // This defaults to 'id' if not changed.
-        
+
             $slicedParentCommentsIds = $slicedParentComments->pluck($modelKeyName)->toArray();
-        
+
             // Remove parent Comments from comments.
             $comments = $comments->where('child_id', '!=', '');
-        
+
             $grouped_comments = new \Illuminate\Pagination\LengthAwarePaginator($slicedParentComments->merge($comments)->groupBy('child_id'), $parentComments->count(), $perPage);
-        
+
             $grouped_comments->withPath(request()->url());
         } else {
             $grouped_comments = $comments->groupBy('child_id');
