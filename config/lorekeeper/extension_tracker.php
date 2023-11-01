fa571b6ad8c361cffcd5023a4630bd5f91cacd3e
jam <ch3rvbcreations@gmail.com>
2023-11-01 11:40:24 -0400

    ext tracker

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -1,6 +1,5 @@
 <?php
 return [
-    
     // FORMAT EXAMPLE.
     //
     // 'extension_tracker' => [
@@ -11,6 +10,14 @@ return [
     //     ]),
     //     'version' => '1.0.0',
     // ],
-    
 
+    'encounters' => [
+        'key' => 'encounters',
+        'wiki_key' => 'Encounters',
+        'creators' => json_encode([
+            'CH3RVB' => 'https://github.com/CH3RVB/',
+            'LostInProgres' => 'https://github.com/LostInProgres',
+        ]),
+        'version' => '1.0.0',
+    ],
 ];
