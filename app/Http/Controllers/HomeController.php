b055f94c9cfb9130fb840f964adb6076e4621a98
Ne-wt <Ne-wt@users.noreply.github.com>
2023-12-30 19:45:03 +0000

    refactor: fix PHP styling

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Laravel\Socialite\Facades\Socialite;
-use Carbon\Carbon;
 
 class HomeController extends Controller {
     /*
