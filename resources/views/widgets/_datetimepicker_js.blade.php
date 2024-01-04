bb843c2f8d4ff5dd0d818eed6d7d338428d65bdf
SpeedyD <SpeedyD@users.noreply.github.com>
2024-01-04 08:29:01 +0000

    refactor: fix blade formatting

--- resources/views/widgets/_datetimepicker_js.blade.php
+++ resources/views/widgets/_datetimepicker_js.blade.php
@@ -6,11 +6,7 @@
             changeMonth: true,
             changeYear: true,
             timezone: '{!! Carbon\Carbon::now()->utcOffset() !!}',
-            {!! isset($dtinline)
-                ? "altField: '." .
-                    $dtinline .
-                    "', altFieldTimeOnly: false,"
-                : null !!}
+            {!! isset($dtinline) ? "altField: '." . $dtinline . "', altFieldTimeOnly: false," : null !!}
         });
         @if (isset($dtvalue))
             $(".datepicker").datetimepicker("setDate", "{!! Carbon\Carbon::parse($dtvalue) !!} {!! Carbon\Carbon::now()->utcOffset() !!}}");
