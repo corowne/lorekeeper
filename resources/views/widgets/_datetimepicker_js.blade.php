0ea24fdf4d433b9c954baaf416e11ef2ba464ba3
SpeedyD <SpeedyD@users.noreply.github.com>
2024-01-04 08:25:35 +0000

    refactor: fix blade formatting

--- resources/views/widgets/_datetimepicker_js.blade.php
+++ resources/views/widgets/_datetimepicker_js.blade.php
@@ -10,7 +10,7 @@
                 ? "altField: '." .
                     $dtinline .
                     "',
-                                            altFieldTimeOnly: false,"
+                                                        altFieldTimeOnly: false,"
                 : null !!}
         });
         @if (isset($dtvalue))
