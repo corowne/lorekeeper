3f48a52ba64af37f6f156269b7690d6ed1785a1c
itinerare <itinerare@users.noreply.github.com>
2024-01-11 14:42:14 +0000

    refactor: fix blade formatting

--- resources/views/auth/passwords/forgot.blade.php
+++ resources/views/auth/passwords/forgot.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    @if(isset($status) || isset($errors))
+    @if (isset($status) || isset($errors))
         <div class="alert alert-success mb-4">
             Form submitted successfully. If this email address is registered to an account, you will receive a password reset email.
         </div>
