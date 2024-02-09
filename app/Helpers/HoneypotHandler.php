1072808ba9b34671aa4d28ac798b00462085e35c
Ne-wt <Ne-wt@users.noreply.github.com>
2024-02-09 16:36:45 +0000

    refactor: fix PHP styling

--- app/Helpers/HoneypotHandler.php
+++ app/Helpers/HoneypotHandler.php
@@ -9,6 +9,7 @@ use Spatie\Honeypot\SpamResponder\SpamResponder;
 class HoneypotHandler implements SpamResponder {
     public function respond(Request $request, Closure $next) {
         flash('Woah there! Your request has activated anti-spam measures. If you believe this to be in error, please wait a few seconds before submitting the form again.')->warning();
+
         return redirect()->back();
     }
 }
