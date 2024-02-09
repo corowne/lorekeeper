fb9f9b24303739b07cdfa9970a48800c57800738
ScuffedNewt <dragonminer101@gmail.com>
2024-02-09 16:34:24 +0000

    fix: add back has_image checks and also change honeypot to return for better UX

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -14,9 +14,11 @@
     <div class="row shops-row">
         @foreach ($shops as $shop)
             <div class="col-md-3 col-6 mb-3 text-center">
-                <div class="shop-image">
-                    <a href="{{ $shop->url }}"><img src="{{ $shop->shopImageUrl }}" alt="{{ $shop->name }}" /></a>
-                </div>
+                @if ($shop->has_image)
+                    <div class="shop-image">
+                        <a href="{{ $shop->url }}"><img src="{{ $shop->shopImageUrl }}" alt="{{ $shop->name }}" /></a>
+                    </div>
+                @endif
                 <div class="shop-name mt-1">
                     <a href="{{ $shop->url }}" class="h5 mb-0">{{ $shop->name }}</a>
                 </div>
