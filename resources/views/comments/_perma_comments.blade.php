09346a255791d16fc6d7e8ed4bd3b4c3379b0b5a
Lexi <lexisario5503@gmail.com>
2024-07-25 13:51:47 -0700

    comment layout fix

--- resources/views/comments/_perma_comments.blade.php
+++ resources/views/comments/_perma_comments.blade.php
@@ -27,10 +27,7 @@
             <div
                 class="comment border p-3 rounded {{ $limit == 0 ? 'shadow-sm border-info' : '' }} {{ $comment->is_featured && $limit != 0 ? 'border-success' : '' }} {{ $comment->likes()->where('is_like', 1)->count() -$comment->likes()->where('is_like', 0)->count() <0? 'bg-light bg-gradient': '' }}">
                 <p>
-                    {!! config('lorekeeper.settings.wysiwyg_comments') ? $comment->comment : '<p>' . nl2br($markdown->line(strip_tags($comment->comment))) . '</p>' !!}
-                </p>
-                <p>
-                    {!! $comment->commentable_type == 'App\Models\Forum' && $comment->id == $comment->topComment->id ? nl2br($comment->comment) : nl2br($markdown->line($comment->comment)) !!}
+                    {!! config('lorekeeper.settings.wysiwyg_comments') ? $comment->commentable_type == 'App\Models\Forum' && $comment->id == $comment->topComment->id ? nl2br($comment->comment) : nl2br($markdown->line($comment->comment)) !!}
                 </p>
                 <p class="border-top pt-1 text-right mb-0">
                     <small class="text-muted">{!! $comment->created_at !!}
--- resources/views/comments/_perma_layout.blade.php
+++ resources/views/comments/_perma_layout.blade.php
@@ -9,20 +9,6 @@
 @endsection
 
 @section('content')
-    <h1>Comments on {!! $comment->commentable_type == 'App\Models\User\UserProfile' ? $comment->commentable->user->displayName : $comment->commentable->displayName !!}</h1>
-    <h5>
-        @if (isset($comment->child_id))
-            <a href="{{ url('comment/') . '/' . $comment->child_id }}" class="btn btn-secondary btn-sm mr-2">See Parent</a>
-            <a href="{{ url('comment/') . '/' . $comment->topComment->id }}" class="btn btn-secondary btn-sm mr-2">Go To Top Comment</a>
-        @endif
-    </h5>
-
-    <hr>
-    <br>
-    <div class="d-flex mw-100 row mx-0" style="overflow:hidden;">
-        @include('comments._perma_comments', ['comment' => $comment, 'limit' => 0, 'depth' => 0])
-    </div>
-
 
     <h1>
         Comments on
@@ -46,6 +32,7 @@
     <div class="d-flex mw-100 row mx-0" style="overflow:hidden;">
         @include('comments._perma_comments', ['comment' => $comment, 'limit' => 0, 'depth' => 0])
     </div>
+    
 @endsection
 @section('scripts')
     <script>
