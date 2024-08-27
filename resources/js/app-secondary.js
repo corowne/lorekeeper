5843b5080e349783bb04c7b3aee2d9ff54bed628
itinerare <ad@itinerare.net>
2024-08-27 09:15:06 -0400

    chore(deps-dev): set up mix, rebuild assets - update bootstrap to 4.6 - update some included JS deps - move some included JS deps to be handled by mix - defer loading non-critical JS and CSS - add mix build to CI workflow, rename - clean up JS deps - remove redundant lockfile - use consistent formatting for masterlist images toggle (fixes label displaying incorrectly)

---
+++ resources/js/app-secondary.js
@@ -0,0 +1,8 @@
+/**
+ * This is a secondary file handling dependencies which are non-critical
+ * and can be deferred. However, it is still practical to handle them
+ * via mix so that they can be processed at once and minified.
+ */
+
+require('./bootstrap4-toggle');
+require('./lightbox');
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,10 @@
  */
 
 require('./bootstrap');
+require('./jquery-ui');
+require('./selectize');
 
-window.Vue = require('vue');
+//window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +22,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +30,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+//const app = new Vue({
+//    el: '#app'
+//});
