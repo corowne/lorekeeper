eae93361760cab559f5d6474ddf0bd46032d2fb2
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2024-08-28 16:44:13 +0000

    refactor: fix PHP styling

--- database/migrations/2024_08_28_103434_convert_character_subtypes.php
+++ database/migrations/2024_08_28_103434_convert_character_subtypes.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         //
         if (!Schema::hasTable('character_image_subtypes')) {
             Schema::create('character_image_subtypes', function (Blueprint $table) {
@@ -27,8 +25,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         //
         Schema::dropIfExists('character_image_subtypes');
 
