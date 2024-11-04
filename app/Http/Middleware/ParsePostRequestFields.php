2ac86c2d04ea9c14fd7b253d61edfaf130f4a3d6
Pasta <66919180+notecardPasta@users.noreply.github.com>
2024-11-04 07:19:44 -0500

    fix: add criteria to fields excluded from parsing (#1105)

--- app/Http/Middleware/ParsePostRequestFields.php
+++ app/Http/Middleware/ParsePostRequestFields.php
@@ -15,7 +15,7 @@ class ParsePostRequestFields {
      */
     public function handle(Request $request, Closure $next) {
         if ($request->isMethod('post')) {
-            $excludedFields = ['_token', 'password', 'email', 'description', 'text'];
+            $excludedFields = ['_token', 'password', 'email', 'description', 'text', 'criteria'];
             $strippedFields = ['name', 'title'];
 
             $parsedFields = [];
