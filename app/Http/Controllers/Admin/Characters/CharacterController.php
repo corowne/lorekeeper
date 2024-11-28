6e03d32630be48454ab60aee4b51f4dbebbb4749
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2024-11-28 10:30:52 +0000

    refactor: fix PHP styling

--- app/Http/Controllers/Admin/Characters/CharacterController.php
+++ app/Http/Controllers/Admin/Characters/CharacterController.php
@@ -14,7 +14,6 @@ use App\Models\Species\Species;
 use App\Models\Species\Subtype;
 use App\Models\Trade;
 use App\Models\User\User;
-use App\Models\User\UserItem;
 use App\Services\CharacterManager;
 use App\Services\TradeManager;
 use Illuminate\Http\Request;
